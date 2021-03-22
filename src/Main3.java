4e1fd8c18ba8ff2fd6d35ef005d725e855795556
istoric564 <mrdizzy38@gmail.com>
2021-03-23 00:48:44 +0700

    Эта игра "Быки и Коровы". Правила очень просты и не притязательны. Играет пользователь и компьютер , последний загадывает четыре цифры без повторений. Например, это может быть число 1834. Число НЕ МОЖЕТ начинаться с нуля. Консольный вариант. Если есть замечания по коду - всегда рад слышать. This game is "Bulls and Cows". The rules are very simple and not pretentious. The user and the computer play, the latter guessing four numbers without repetition. For example, it can be the number 1834. The number CANNOT start from zero. Console version. If you have any comments on the code , I am always happy to hear them.

---
+++ src/Main3.java
@@ -0,0 +1,108 @@
+
+
+import java.util.*;
+
+
+public class Main3 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int[] computerNumbers = new int[4];
+        int[] userAnswer = new int[4];
+        int countBulls;
+        int countCows;
+
+        for (int i = 0; i < computerNumbers.length; i++) {
+            computerNumbers[i] = (int) (Math.random()*8+1);
+        }
+        // Настройка рандомного значения компьютера.
+        if(computerNumbers[0] == 0){
+            computerNumbers[0]++;
+        }
+        for (int i = 0; i < computerNumbers.length; i++) {
+            for (int j = i + 1; j < computerNumbers.length; j++) {
+                if(computerNumbers[i] == computerNumbers[j]){
+                    computerNumbers[j]++;
+                }
+            }
+        }
+        for (int i = 0; i < computerNumbers.length; i++) {
+            System.out.print(computerNumbers[i]);
+        }
+        System.out.println(" Игра Быки и коровы");
+        System.out.println(" Тебе нужно ввести четыре числа от 1 до 9 без повторяющихся чисел ");
+        System.out.println(" У тебя безлимитное количество попыток. Удачи победитель :)");
+
+        for(int userAttempts = 1; ; userAttempts++) {
+            countBulls = 0;
+            countCows = 0;
+            System.out.println(userAttempts + ") Пожалуйста введите свое число: ");
+            try{
+                //        Заполнение массива пользователя
+                for (int i = 0; i < userAnswer.length; i++) {
+                    userAnswer[i] = scanner.nextInt();
+                }
+            } catch (NumberFormatException exception){
+                System.err.println("Ошибка: введены не числовые символы");
+//                Уменьшаем счетчик попыток
+                userAttempts--;
+                continue;
+            }for (int k : userAnswer) {
+                if ((k < 0 || k > 9)) {
+                    System.out.println("Ошибка: введено число меньше 1 или больше 9");
+//                Уменьшаем счетчик попыток
+                    userAttempts--;
+                    continue;
+                }
+            }
+            try {
+                for (int i = 0; i < 4; i++) {
+                    for (int j = i +1; j < i; j++) {
+                        if (userAnswer[i] == userAnswer[j]) {
+                            System.out.println("Ошибка: введено число с дублем.");
+                            userAttempts--;
+                            throw new Exception();
+                        }
+                    }
+                }
+            }catch (Exception exception){
+                continue;
+            }
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < userAnswer.length; i++) {
+                list.add(i,userAnswer[i]);
+            }
+//        Проверка значения быков.
+            for (int i = 0; i < computerNumbers.length; i++) {
+                int c = computerNumbers[i];
+                if (c == userAnswer[i]) {
+                    countBulls++;
+                }else if (list.contains(computerNumbers[i])){
+                    countCows++;
+                }
+            }
+//        Проверка значения коров.
+//            for (int i = 0; i < userAnswer.length; i++) {
+//                if (list.contains(computerNumbers[i])){
+//                    countCows++;
+//                }
+//            }
+//            for (int j : userAnswer) {
+//                System.out.print(j);
+//            }
+//            System.out.println("|");
+//            for (int computerNumber : computerNumbers) {
+//                System.out.print(computerNumber);
+//            }
+//            System.out.println("|");
+
+            System.out.println("Bulls: " + countBulls);
+            System.out.println("Cows: " + countCows);
+            if (countBulls == 4) {
+                break;
+            }
+        }
+        System.out.println("Ты победил, ура!");
+
+    }
+}
+
